7d74497e83874e58fb83efa0f851a0d6bc97fb74
Lucas <lucasleitep15@gmail.com>
2025-11-15 06:44:15 -0300

    Update script-cadastro-vitima

--- back-end/core/static/front/script-cadastro.js
+++ back-end/core/static/front/script-cadastro.js
@@ -121,7 +121,7 @@ document.addEventListener('DOMContentLoaded', function () {
             
             if (data.success) {
                 alert('Cadastro realizado com sucesso! Entraremos em contato em breve.');
-                window.location.href = "{% url 'index' %}";
+                window.location.href = "/";
             } else {
                 throw new Error(data.message || 'Erro ao cadastrar');
             }

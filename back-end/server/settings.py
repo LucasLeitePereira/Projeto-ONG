f1a2c8d264846c5f87e47f013964ad60667eb514
Lucas <lucasleitep15@gmail.com>
2025-11-15 02:57:19 -0300

    Preparando para migração

--- back-end/server/settings.py
+++ back-end/server/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -31,9 +31,12 @@ INSTALLED_APPS = [
     'voluntarios',
     'vitimas',
     'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -42,6 +45,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost',
+    'http://127.0.0.1:5500',
+]
+
+CORS_ALLOW_HEADERS = ["*"]
+CORS_ALLOW_METHODS = ["*"]
 
 ROOT_URLCONF = 'server.urls'
 
@@ -97,14 +108,17 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
 USE_TZ = True
 
+DATE_FORMAT = 'd/m/Y'
+DATETIME_FORMAT = 'd/m/Y H:i:s'
+SHORT_DATE_FORMAT = 'd/m/Y'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
--- back-end/vitimas/views.py
+++ back-end/vitimas/views.py
@@ -15,12 +15,10 @@ def adicionar(request):
             cep = data.get('cep')
             idade = data.get('idade')
             apelido = data.get('apelido')
-            num_apelido = data.get('num_apelido')
             cidade = data.get('cidade')
             estado = data.get('estado')
             rua = data.get('rua')
             num_endereco = data.get('num_endereco')
-            complemento_endereco = data.get('complemento_endereco')
 
             vitima = Vitima.objects.create(
                 cpf_vitima= formatar_cpf(cpf),
@@ -28,12 +26,10 @@ def adicionar(request):
                 cep_vitima=cep,
                 idade_vitima=idade,
                 apelido_vitima=apelido,
-                num_apelido_vitima=num_apelido,
                 cidade_vitima=cidade,
                 estado_vitima=estado,
                 rua_vitima=rua,
                 num_endereco_vitima=num_endereco,
-                complemento_endereco_vitima=complemento_endereco
             )
             
             return JsonResponse({

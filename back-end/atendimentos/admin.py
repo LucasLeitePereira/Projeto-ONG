8f8f12ce28bdf05b251c93a3004c1bc831487be1
Lucas <lucasleitep15@gmail.com>
2025-11-15 21:57:24 -0300

    Criação do Atendimento

--- back-end/atendimentos/admin.py
+++ back-end/atendimentos/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from .models import Atendimento
 
-# Register your models here.
+
+@admin.register(Atendimento)
+class AtendimentoAdmin(admin.ModelAdmin):
+    list_display = (
+        'id_atendimento',
+        'get_nome_vitima',
+        'get_nome_estagiario',
+        'data_atendimento',
+        'hora_atendimento'
+    )
+    
+    list_filter = (
+        'data_atendimento',
+        'id_estagiario'
+    )
+    
+    search_fields = (
+        'id_vitima__nome_vitima',
+        'id_vitima__cpf_vitima',
+        'id_estagiario__id_voluntario__nomecompleto_voluntario'
+    )
+    
+    date_hierarchy = 'data_atendimento'
+    
+    fieldsets = (
+        ('Informações da Vítima', {
+            'fields': ('id_vitima',)
+        }),
+        ('Estagiário Responsável', {
+            'fields': ('id_estagiario',)
+        }),
+        ('Data e Hora', {
+            'fields': ('data_atendimento', 'hora_atendimento')
+        }),
+    )
+    
+    # Métodos personalizados para exibição
+    def get_nome_vitima(self, obj):
+        return obj.id_vitima.nome_vitima
+    get_nome_vitima.short_description = 'Vítima'
+    get_nome_vitima.admin_order_field = 'id_vitima__nome_vitima'
+    
+    def get_nome_estagiario(self, obj):
+        return obj.id_estagiario.id_voluntario.nomecompleto_voluntario
+    get_nome_estagiario.short_description = 'Estagiário'
+    get_nome_estagiario.admin_order_field = 'id_estagiario__id_voluntario__nomecompleto_voluntario'
+    
+    # Configurações adicionais
+    save_on_top = True
+    list_per_page = 25
--- back-end/atendimentos/models.py
+++ back-end/atendimentos/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+class Atendimento(models.Model):
+    id_atendimento = models.AutoField(primary_key=True)
+    
+    id_vitima = models.ForeignKey(
+        'vitimas.Vitima', 
+        on_delete=models.CASCADE,
+        db_column='id_vitima',
+        related_name='atendimentos'
+    )
+    
+    id_estagiario = models.ForeignKey(
+        'voluntarios.Estagiario',
+        on_delete=models.CASCADE,
+        db_column='id_estagiario',
+        related_name='atendimentos',
+        null=True,
+        blank=True
+    )
+    
+    id_advogado = models.ForeignKey(
+        'voluntarios.Advogado',
+        on_delete=models.CASCADE,
+        db_column='id_advogado',
+        related_name='atendimentos',
+        null=True,
+        blank=True
+    )
+    
+    # Campos do atendimento
+    data_atendimento = models.DateField()
+    hora_atendimento = models.TimeField()
+    
+    class Meta:
+        db_table = 'Atendimentos'
+        verbose_name = 'Atendimento'
+        verbose_name_plural = 'Atendimentos'
+        ordering = ['-data_atendimento']
+    
+    def __str__(self):
+        return f"Atendimento #{self.id_atendimento} - {self.id_vitima.nome_vitima}"
